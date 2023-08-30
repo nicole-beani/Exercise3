e6aea8f45d93d98fc8617d4e7e32720509cdb44f
Nicole <129675447+nicole-beani@users.noreply.github.com>
2023-08-30 16:45:06 +0200

    Add files via upload

---
+++ Exercise3/ContoCorrente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class ContoCorrente
+    {
+        private string Nome { get; set; }
+        private string Cognome { get; set; }
+        public double Saldo { get; set; }
+
+        public bool ContoAperto = false;
+
+        public void AprireConto(double tot)
+        {
+            if (ContoAperto) { Console.WriteLine("Conto già aperto!"); }
+            else
+            {
+                Saldo = 0;
+                ContoAperto = true;
+                if (tot >= 1000) Versamento(tot);
+                else Console.WriteLine("Aggiungi soldi");
+            }
+        }
+        public double Versamento(double tot)
+        {
+            return Saldo += tot;
+        }
+        public double Prelievo(double tot)
+        {
+            return Saldo -= tot;
+        }
+        public string StatoDelConto()
+        {
+            return $"Il conto è intestato a {Nome} {Cognome}. Il Saldo totale è {Saldo}";
+        }
+        public ContoCorrente() { }
+        public ContoCorrente(string Nome, string Cognome)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+        }
+    }
+}
---
+++ Exercise3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisic il tuo nome");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo Cognome");
+            string Cognome = Console.ReadLine();
+            ContoCorrente NomeConto = new ContoCorrente(Nome, Cognome);
+            int choice = 0;
+            while (choice != 5)
+            {
+                Console.WriteLine("SCEGLI L'OPERAZIONE DA EFFETTUARE");
+                Console.WriteLine("1 --- Apri un Nuovo Conto Corrente");
+                Console.WriteLine("2 --- Esegui un Versamento");
+                Console.WriteLine("3 --- Effettua un Prelievo");
+                Console.WriteLine("4 --- Mostra Saldo");
+                Console.WriteLine("5 --- Esci");
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine("Per aprire un nuovo conto ti servono minimo 1000 euro, inserisci l'ammontare");
+                    double Ammontare = Convert.ToDouble(Console.ReadLine());
+                    NomeConto.AprireConto(Ammontare);
+
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Quanto vuoi Depositare?");
+                    double Ammontare = Convert.ToDouble(Console.ReadLine());
+                    NomeConto.Versamento(Ammontare);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Quanto vuoi Prelevare?");
+                    double SoldiPrelevati = Convert.ToDouble(Console.ReadLine());
+                    NomeConto.Prelievo(SoldiPrelevati);
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine($"Il tuo Saldo è {NomeConto.Saldo}");
+
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Ritirare la carta entro 30 secondi, l'operazione è stata conclusa");
+                }
+                else
+                {
+                    Console.WriteLine("La scelta non è valida");
+                }
+
+            }
+
+
+        }
+    }
+}
